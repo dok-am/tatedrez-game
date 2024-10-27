c19d130dd1a22bab77bf39dd2ece491e90c66792
Andrei Maistruk <dok@ironictitle.com>
2024-10-27 21:48:33 +0400

    added camera fit

--- Assets/Tatedrez/Scripts/View/BoardView/BoardViewController.cs
+++ Assets/Tatedrez/Scripts/View/BoardView/BoardViewController.cs
@@ -1,4 +1,4 @@
-﻿using System.Drawing;
+﻿using System;
 using Tatedrez.Application;
 using Tatedrez.Data.Interfaces;
 using UnityEngine;
@@ -7,12 +7,21 @@ namespace Tatedrez.View.BoardView
 {
     public class BoardViewController 
     {
+        public event Action<BoardViewController> OnBoardBuilded;
+
+        public float CellSize => _cellSize;
+        public float GridSize => _gridSize;
+        public float BoardTotalSize => _cellSize * _gridSize;
+
         private BoardCellView _cellPrefab;
         private Sprite _whiteCellSprite;
         private Sprite _blackCellSprite;
 
         private BoardCellView[] _gridView;
         private Transform _gridContainer;
+
+        private float _cellSize;
+        private float _gridSize;
 
         public BoardViewController(GameplaySceneBinder sceneBinder)
         {
@@ -23,15 +32,20 @@ namespace Tatedrez.View.BoardView
 
         public void BuildBoard(IBoard board)
         {
+            if (_gridContainer != null || _gridView != null)
+                throw new Exception("Can't rebuild board on runtime");
+
             _gridContainer = new GameObject("BOARD").transform;
             _gridContainer.position = Vector3.zero;
 
             int arraySize = board.Size * board.Size;
             _gridView = new BoardCellView[arraySize];
 
-            float cellSize = _cellPrefab.Size;
-            float halfBoardSize = (cellSize * board.Size) / 2.0f;
-            float halfCellSize = (cellSize / 2.0f);
+            _cellSize = _cellPrefab.Size;
+            _gridSize = board.Size;
+
+            float halfBoardSize = (_cellSize * board.Size) / 2.0f;
+            float halfCellSize = (_cellSize / 2.0f);
             float beginOffsetX = - halfBoardSize + halfCellSize;
             float beginOffsetY = halfBoardSize - halfCellSize;
             Vector2 cellStartPosition = new(beginOffsetX, beginOffsetY);
@@ -43,11 +57,13 @@ namespace Tatedrez.View.BoardView
                 int rowIndex = i / board.Size;
 
                 Vector2 cellPosition = cellStartPosition
-                    + new Vector2(colIndex * cellSize, -rowIndex * cellSize);
+                    + new Vector2(colIndex * _cellSize, -rowIndex * _cellSize);
                 _gridView[i] = CreateBoardCellView(black, cellPosition);
 
                 black = !black;
             }
+
+            OnBoardBuilded?.Invoke(this);
         }
 
         private BoardCellView CreateBoardCellView(bool black, Vector2 position)
---
+++ Assets/Tatedrez/Scripts/View/CameraBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using Tatedrez.View.BoardView;
+using UnityEngine;
+using VContainer;
+
+namespace Tatedrez.View
+{
+    [RequireComponent(typeof(Camera))]
+    public class CameraBehaviour : MonoBehaviour
+    {
+        [SerializeField] private Camera _camera;
+
+        [Inject]
+        public void InjectDependencies(BoardViewController boardViewController)
+        {
+            boardViewController.OnBoardBuilded += OnBoardBuilded;
+        }
+
+        private void OnBoardBuilded(BoardViewController boardViewController)
+        {
+            boardViewController.OnBoardBuilded -= OnBoardBuilded;
+            RecalculateOrthograthicSize(boardViewController.BoardTotalSize);
+        }
+
+        private void RecalculateOrthograthicSize(float boardSize)
+        {
+            _camera.orthographicSize = boardSize / 2.0f / _camera.aspect;
+        }
+
+        private void Reset()
+        {
+            _camera = GetComponent<Camera>();
+        }
+
+        private void OnValidate()
+        {
+            if (_camera == null)
+                _camera = GetComponent<Camera>();
+        }
+    }
+}
